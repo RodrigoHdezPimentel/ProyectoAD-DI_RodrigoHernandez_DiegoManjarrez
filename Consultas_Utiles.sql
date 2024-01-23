b768d2ff1d0c7179d3085636fe3a97fe39dbc8ba
Rodrigo <rodrigo.hdez.oficial@gmail.com>
2024-01-23 09:13:17 +0100

    Proyecto java para crear inserts aleatorios; e inserts usuario_tema creados

--- Consultas_Utiles.sql
+++ Consultas_Utiles.sql
@@ -1,4 +1,18 @@
-select * from Chat where idUsuarioDestino = IdUsuarioOrigen;
+select * from Chat;
+select * from Usuarios;
+select * from Temas;
+
+#Borrar los registros de users menores de edad viendo contenido inapropiado
+delete from usuario_tema 
+	where (idUsuario, idTema) in (
+		select us.idUsuario, te.idTema from usuarios us 
+			Join usuario_tema ut on ut.idUsuario = us.idUsuario 
+				join temas te on te.idTema = ut.idTema where te.edadMinima = 18 and us.edad < 18);
+
+#Comprobar que ningun menor de edad vea contenido inapropiado
+select us.idUsuario, te.idTema from usuarios us 
+	Join usuario_tema ut on ut.idUsuario = us.idUsuario 
+		join temas te on te.idTema = ut.idTema where te.edadMinima = 18 and us.edad < 18;
 
 
 select * from Chat where (idUsuarioOrigen = 1 && IdUsuarioDestino = 25) || (IdUsuarioDestino = 1 && idUsuarioOrigen = 25) order by fecha;
---
+++ InsertBD/src/Inserts/insertChat.java
@@ -0,0 +1,77 @@
+package Inserts;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
+public class insertChat {
+
+	public static void main(String[] args) {
+		File lorem = new File("lorem.txt");
+		try {
+			FileReader fr = new FileReader(lorem);
+			BufferedReader br = new BufferedReader(fr);
+
+			String linea = "";
+			String texto = "";
+
+			while ((linea = br.readLine()) != null) {
+				if (!linea.equals("")) {;
+					texto += linea;
+				}
+			}
+			br.close();
+			fr.close();
+			
+			
+			String[] textoSplit = texto.split("\\. ");
+			for (String l : textoSplit) {
+				System.out.println(l);
+			}
+
+			File sql = new File("insertChat.txt");
+			if (!sql.exists()) {
+				sql.createNewFile();
+			}
+			FileWriter fw = new FileWriter(sql);
+			PrintWriter pw = new PrintWriter(fw);
+
+			Random rm = new Random();
+			int destino;
+			int origen;
+			String contenido;
+			String fecha;
+			for (int i = 0; i < 1000; i++) {
+				destino = rm.nextInt(50) + 1;
+				origen = rm.nextInt(50) + 1;
+				if (destino != origen) {
+
+					int dia = rm.nextInt(30) + 1;
+					int mes = rm.nextInt(12) + 1;
+					if (mes == 2 && dia > 28) {
+						i--;
+
+					}
+					fecha = rm.nextInt(2000, 2024) + "-" + String.format("%02d", mes) + "-"
+							+ String.format("%02d", dia);
+					contenido = textoSplit[(rm.nextInt(textoSplit.length - 1))];
+					pw.print("(" + origen + ", " + destino + ", \'" + fecha + "\', \'" + contenido + "\'),\n");
+				}
+			}
+			pw.flush();
+			pw.close();
+			fw.close();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+	}
+
+}
