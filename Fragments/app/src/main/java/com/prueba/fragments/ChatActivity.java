b182f6551ef552b25d69a703a8ae04540475e319
Rodrigo <rodrigo.hdez.oficial@gmail.com>
2024-02-11 14:56:56 +0100

    (develope) Conversation interface start, need to see the return to chat list intent

--- Fragments/app/src/main/java/com/prueba/fragments/ChatActivity.java
+++ Fragments/app/src/main/java/com/prueba/fragments/ChatActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import com.prueba.fragments.Fragments.MainFragment.Chats;
 import com.prueba.fragments.RecyclerViews.Adapters.ChatRvAdapter;
 import com.prueba.fragments.RecyclerViews.Adapters.ListaChatsRvAdapter;
 
@@ -21,5 +26,14 @@ public class ChatActivity extends AppCompatActivity {
         ChatRvAdapter adapter = new ChatRvAdapter(this, Login_SignUP.chatConversation);
         MyRecyclerView.setAdapter(adapter);
         MyRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ImageView arrow = findViewById(R.id.arrow);
+        arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent listChat = new Intent(ChatActivity.this, MainActivity.class);
+                startActivity(listChat);
+            }
+        });
     }
 }
